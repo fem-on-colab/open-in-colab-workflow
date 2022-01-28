7d9ba0999ff183e488ab1f7b4936e84f482485a5
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-28 09:35:17 +0100

    Make replace_images_in_markdown package runnable as a script

--- open_in_colab_workflow/replace_images_in_markdown.py
+++ open_in_colab_workflow/replace_images_in_markdown.py
@@ -7,9 +7,13 @@
 
 import copy
 import os
+import sys
 import typing
 
 import nbformat
+
+from open_in_colab_workflow.glob_files import glob_files
+from open_in_colab_workflow.glob_images import glob_images
 
 
 def replace_images_in_markdown(
@@ -26,3 +30,18 @@ def replace_images_in_markdown(
         else:
             updated_nb_cells.append(cell)
     return updated_nb_cells
+
+
+if __name__ == "__main__":  # pragma: no cover
+    assert len(sys.argv) == 3
+    work_dir = sys.argv[1]
+    nb_pattern = sys.argv[2]
+
+    images_as_base64 = glob_images(work_dir)
+
+    for nb_filename in glob_files(work_dir, nb_pattern):
+        with open(nb_filename, "r") as f:
+            nb = nbformat.read(f, as_version=4)
+        nb.cells = replace_images_in_markdown(nb.cells, os.path.dirname(nb_filename), images_as_base64)
+        with open(nb_filename, "w") as f:
+            nbformat.write(nb, f)
