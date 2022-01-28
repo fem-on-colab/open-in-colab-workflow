f5ab25976be7fd44b1b27d42ad410eef21adccab
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-28 09:48:02 +0100

    Make upload_files_to_google_drive package runnable as a script

--- open_in_colab_workflow/upload_files_to_google_drive.py
+++ open_in_colab_workflow/upload_files_to_google_drive.py
@@ -5,9 +5,12 @@
 # SPDX-License-Identifier: MIT
 """Upload all files matching at least one pattern to Google Drive."""
 
+import sys
+
 import docker
 
 from open_in_colab_workflow.get_rclone_env import get_rclone_env
+from open_in_colab_workflow.publish_on import publish_on, PublishOnDrive
 
 
 def upload_files_to_google_drive(work_dir: str, pattern: str, drive_root_directory: str) -> None:
@@ -32,3 +35,13 @@ def _upload_files_with_rclone(work_dir: str, pattern: str, drive_root_directory:
         "rclone/rclone", f"-q copy /data colab:{drive_root_directory} --include {pattern}",
         environment=rclone_env, volumes=volumes, remove=True
     )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    assert len(sys.argv) == 4
+    work_dir = sys.argv[1]
+    upload_pattern = sys.argv[2]
+    publisher = publish_on(sys.argv[3])
+    assert isinstance(publisher, PublishOnDrive)
+
+    upload_files_to_google_drive(work_dir, upload_pattern, publisher.drive_root_directory)
