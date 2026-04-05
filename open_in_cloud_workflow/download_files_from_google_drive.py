ec0e9b3a82c8f20564ee5fb97886aef045af4bbc
Francesco Ballarin <francesco.ballarin@unicatt.it>
2026-04-05 11:02:02 +0200

    Add script to download files from google drive

---
+++ open_in_cloud_workflow/download_files_from_google_drive.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2021-2026 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab-related actions.
+#
+# SPDX-License-Identifier: MIT
+"""Download all files matching at least one pattern to Google Drive."""
+
+import subprocess
+import sys
+
+from open_in_cloud_workflow.get_rclone_env import get_rclone_env
+from open_in_cloud_workflow.publish_on import publish_on, PublishOnDrive
+
+
+def download_files_from_google_drive(work_dir: str, pattern: str, drive_root_directory: str) -> None:
+    """Download all files matching at least one pattern to Google Drive."""
+    subprocess.check_call(
+        (
+            f"rclone -q sync drive:{drive_root_directory} {work_dir} "
+            + " ".join(f"--include {pattern_}" for pattern_ in pattern.strip("\n").split("\n"))
+        ).split(" "),
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, env=get_rclone_env())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    assert len(sys.argv) == 4
+    work_dir = sys.argv[1]
+    download_pattern = sys.argv[2]
+    publisher = publish_on(sys.argv[3])
+    assert isinstance(publisher, PublishOnDrive)
+
+    download_files_from_google_drive(work_dir, download_pattern, publisher.drive_root_directory)
---
+++ tests/test_download_files_from_google_drive.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2021-2026 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab-related actions.
+#
+# SPDX-License-Identifier: MIT
+"""Tests for the open_in_cloud_workflow.upload_files_from_google_drive package."""
+
+import os
+import tempfile
+
+import pytest
+import requests
+
+from open_in_cloud_workflow.download_files_from_google_drive import download_files_from_google_drive
+from open_in_cloud_workflow.get_drive_url import get_drive_url
+
+
+def assert_files_equal(root_directory: str, pattern: str, url: str) -> None:
+    """Assert that the local file and the downloaded one have the same content."""
+    remote_data = requests.get(url.replace("/open?", "/uc?"))
+    remote_data.raise_for_status()
+    with open(os.path.join(root_directory, pattern)) as f:
+        assert f.read() == remote_data.content.decode("utf-8")
+
+
+@pytest.mark.skipif("RCLONE_CONFIG_DRIVE_TOKEN" not in os.environ, reason="Missing rclone environment variables")
+def test_download_files_from_google_drive(root_directory: str) -> None:
+    """Test that updating an existing file on Google Drive preserves its url."""
+    download_pattern = os.path.join("tests", "data", "download_files_from_google_drive", "*.txt")
+    check_pattern = os.path.join("tests", "data", "download_files_from_google_drive", "existing_file.txt")
+    with tempfile.TemporaryDirectory(dir=root_directory) as tmp_root_directory:
+        os.makedirs(os.path.dirname(os.path.join(tmp_root_directory, download_pattern)))
+        download_files_from_google_drive(tmp_root_directory, download_pattern, "GitHub/open_in_colab_workflow")
+        url = get_drive_url(check_pattern, "GitHub/open_in_colab_workflow")
+        assert url == "https://drive.google.com/open?id=1cvMOE2HbDxzQSK6fH4frs0ifZflDsaJY"
+        assert_files_equal(tmp_root_directory, check_pattern, url)
